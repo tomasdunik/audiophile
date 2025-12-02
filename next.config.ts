c864d87cf72fbe81366dbd3df56b4237d861f9ea
Tomáš Duník <tomdunik@gmail.com>
2025-12-02 09:36:20 +0100

    feat: :heavy_plus_sign: pridaná časť s produktami na Homepage

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // vytvorí ./out pri build-e
   images: {
     formats: ["image/avif", "image/webp"],
-    unoptimized: true,
+    //unoptimized: true,
   },
 };
 
