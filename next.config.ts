e68d087569fc83cd788afcb2311905237fc63db5
Tomáš Duník <tomdunik@gmail.com>
2025-12-05 14:46:07 +0100

    add github actions

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     //unoptimized: true,
   },
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/audiophile" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/audiophile/" : "",
 };
 
 export default nextConfig;
