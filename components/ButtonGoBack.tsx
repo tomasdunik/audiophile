ff346f83adfe9782d1e06bdc8b84281dd1065139
Tomáš Duník <tomdunik@gmail.com>
2025-12-10 14:53:55 +0100

    upraveny button goBack

--- components/ButtonGoBack.tsx
+++ components/ButtonGoBack.tsx
@@ -17,7 +17,7 @@ const ButtonGoBack = () => {
     <div className="mt-4 mb-6 px-6 md:mt-[33px] md:px-10 lg:mx-auto lg:mt-[79px] lg:mb-14 lg:max-w-[1190px]">
       <button
         onClick={handleGoBack}
-        className="text-black-50 text-md inline-block leading-25 font-medium"
+        className="text-black-50 text-md hover:text-orange hover:text-orange inline-block cursor-pointer leading-25 font-medium transition-colors duration-150 ease-out"
       >
         Go Back
       </button>
